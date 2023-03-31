03ef51e0dcbe83eaafa677f491a7021a9b2279e7
Iris Veress <68661896+VeressIris@users.noreply.github.com>
2023-03-31 21:55:44 +0300

    initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController characterController;
+    [SerializeField] private float moveSpeed = 10f;
+    
+    private AudioSource audio;
+    //RANDOM FOOTSTEPS???
+    [SerializeField] private AudioClip footstepSound;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        audio = GetComponent<AudioSource>();
+        audio.clip = footstepSound;
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        characterController.Move(move * moveSpeed * Time.deltaTime);
+
+        //Footsteps
+        if(!audio.isPlaying && characterController.velocity.magnitude > 2f)
+        {
+            audio.volume = Random.Range(0.55f, 1.2f);
+            audio.Play();
+        }
+    }
+}
