8fc764270c7ad043af76515a1b12d424918716fe
Iris Veress <68661896+VeressIris@users.noreply.github.com>
2023-04-01 19:25:59 +0300

    senses working
    knife still not working

--- Assets/Scripts/Scalpel.cs
+++ Assets/Scripts/Scalpel.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Scalpel : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float cooldown = 10f;
+
+    [SerializeField] Transform[] spawnPoints;
+    public Vector3 spawnPoint;
+    private Vector3 initialPos;
+    bool called = false;
+
     void Start()
     {
-        
+        PickPoint();
+        initialPos = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!called)
+        {
+            StartCoroutine(Animate());
+        }
+    }
+
+    void PickPoint()
+    {
+        int index = Random.Range(0, spawnPoints.Length);
+        spawnPoint = spawnPoints[index].position;
+    }
+
+    IEnumerator Animate()
+    {
+        called = true;
+
+        StartCoroutine(MoveToTarget(spawnPoint));
+
+        yield return new WaitForSeconds(2f);
+
+        StartCoroutine(MoveToTarget(initialPos));
+
+        yield return new WaitForSeconds(cooldown);
+        called = false;
+
+        PickPoint();
+    }
+
+    IEnumerator MoveToTarget(Vector3 target)
+    {
+        while (transform.position != target)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+            yield return null; //wait a frame
+        }
     }
 }
